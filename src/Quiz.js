506eab3c38be5e11fe4640d5ac296cf9c3027f84
KaiYu Chen <kaiyuchen53@gmail.com>
2022-11-13 10:52:23 -0500

    module 6 again

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,7 +10,7 @@ class Quiz extends Component {
 		this.state = {quiz_position: 1}
 	}
 	showNextQuestion(){
-		this.setState(quiz_position+1)
+		this.setState({quiz_position: quiz_position+1})
 	}
 
 	render(){
